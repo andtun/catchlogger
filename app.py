681f0bf85c80439df955c1962f1a96dfa19c1579
Alexander Tyunyatkin <Alexander Tyunyatkin>
2017-04-08 18:06:04 +0300

    new_template

--- app.py
+++ app.py
@@ -4,12 +4,20 @@ import os
 def page_file(root, filename):
     return static_file(filename, root=root)
 
+def html(filename):
+	filename += ".html"
+	return static_file(filename, root="./html/")
+
 @get("/")
 def man():
-    return static_file("index.html", root=".")
+    return html("index")
+
+@get("/login")
+def login():
+	return html("login")
 
 @route("/<root>/<filename>")
 def f(root, filename):
     return page_file(root, filename)
 
-run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
+run(host="0.0.0.0", port=os.environ.get('PORT', 5000), debug=True)

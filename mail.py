5bf996b10d1d680755b6f3e4d3db6a83729cb583
Andrey Tunyatkin <andtun@yandex.ru>
2018-02-07 14:07:05 +0300

    ssl fix

--- mail.py
+++ mail.py
@@ -15,7 +15,7 @@ Location:
 latitude: %s
 longitude: %s
 Radius: %s
-Location link: <a href="http://catchlogger.herokuapp.com/locvar_access?lat=%s&lng=%s&rad=%s">here</a>
+Location link: <a href="http://cl.jkdev.ru/locvar_access?lat=%s&lng=%s&rad=%s">here</a>
 
 IP = %s
 Provider: %s
